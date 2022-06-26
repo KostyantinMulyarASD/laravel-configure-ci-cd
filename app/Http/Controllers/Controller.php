3445c8909d9a18459a17aeacbe45d5ef2f6fbd18
KostyantinMulyarASD <kostyantin.muliar@asd.team>
2022-06-26 18:35:46 +0300

    test phpstan

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function __construct(
+        public string $i,
+    )
+    {
+        $var = 1;
+        $b = 213;
+        $c = $b*3;
+
+        $responseContents = 45566;
+
+
+
+        $d = $responseContents ?? null;
+    }
 }
